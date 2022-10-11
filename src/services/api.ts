d835cf383150ffc181508e621932766ec031799a
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-10-11 11:34:23 -0300

    [*] changed port api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiCarSell = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'http://localhost:3100',
 })
 
 apiCarSell.interceptors.request.use(async (config: any) => {
