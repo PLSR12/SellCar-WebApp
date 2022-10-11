9cdf9f926f46e445c16f6108f272db0e6d09438a
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-10-11 07:36:59 -0300

    [*] updated Home Page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,106 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import * as Organisms from 'components/Organisms'
+import * as Molecules from 'components/Molecules'
+import api from 'services/api'
+import formatNumber from 'common/utils/formatNumber'
+import * as S from './styles'
 
 export function Home() {
-  return <h1>Hello Wordl</h1>
+  const [categories, setCategories] = useState([])
+  const [activeCategory, setActiveCategory] = useState(0)
+  const [cars, setCars] = useState([])
+  const [filteredCars, setFilteredCars] = useState([])
+  const [loadingCars, setLoadingCars] = useState(true)
+  const [loadingCategories, setLoadingCategories] = useState(true)
+
+  useEffect(() => {
+    const fetchCars = async () => {
+      const { data: categories } = await api.get('categories')
+      const { data: cars } = await api.get('cars')
+
+      const allCars = cars.map((car: any) => {
+        return {
+          ...car,
+          formatedKm: formatNumber(car.km),
+        }
+      })
+
+      const newCategories: any = [
+        {
+          id: 0,
+          name: 'Todos',
+          path: 'all-categories.png',
+          url: 'https://i.ibb.co/V9dnSWh/all-categories.png',
+        },
+        ...categories,
+      ]
+
+      newCategories.sort(({ x, y }: any) => {
+        return x.id - y.id
+      })
+
+      setCategories(newCategories)
+      setLoadingCategories(false)
+      setCars(allCars)
+      setLoadingCars(false)
+    }
+
+    fetchCars()
+  }, [])
+
+  useEffect(() => {
+    if (activeCategory === 0) {
+      setFilteredCars(cars)
+    } else {
+      const newFilteredCars = cars.filter(
+        (product: { category_id: number }) =>
+          product.category_id === activeCategory
+      )
+
+      setFilteredCars(newFilteredCars)
+    }
+  }, [activeCategory, cars])
+
+  return (
+    <>
+      <Organisms.Header />
+      <S.Container>
+        <h1>Categorias</h1>
+
+        <S.ContainerCategory>
+          {!loadingCategories ? (
+            categories.map((category: { id: number }) => (
+              <Organisms.CardCategory
+                key={category.id}
+                category={category}
+                activeCategory={activeCategory}
+                setActiveCategory={setActiveCategory}
+              />
+            ))
+          ) : (
+            <Molecules.Loading />
+          )}
+        </S.ContainerCategory>
+
+        <h1>Anunciados</h1>
+
+        <S.ContainerCars>
+          {!loadingCars ? (
+            filteredCars.map((car: { id: number }) => (
+              <Organisms.CardVehicle key={car.id} car={car} />
+            ))
+          ) : (
+            <Molecules.Loading isVehicle />
+          )}
+
+          {!filteredCars.length && !loadingCars && (
+            <S.InfoCar style={{ color: '#fff', fontWeight: '700' }}>
+              Ops... Nenhum veículo anunciado ainda
+            </S.InfoCar>
+          )}
+        </S.ContainerCars>
+      </S.Container>
+    </>
+  )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,54 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  background: #fff;
+
+  min-height: calc(100vh - 60px);
+  padding: 0 20px 50px 20px;
+
+  h1 {
+    color: #fff;
+    text-align: center;
+    padding: 40px 0;
+  }
+`
+
+export const ContainerCategory = styled.div`
+  width: 100%;
+  overflow: auto;
+  overflow-y: hidden;
+  height: 150px;
+  position: relative;
+  padding: 0 50px;
+  display: flex;
+  gap: 20px;
+  justify-content: space-around;
+
+  @media (max-width: 750px) {
+    height: auto;
+    display: flexbox;
+    padding: 0;
+  }
+`
+
+export const ContainerCars = styled.div`
+  width: 100%;
+  position: relative;
+  padding: 0 50px;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 15px;
+  justify-content: space-around;
+
+  @media (max-width: 520px) {
+    padding: 0;
+  }
+`
+
+export const InfoCar = styled.p`
+  display: flex;
+  align-items: center;
+  text-align: center;
+  font-size: 18px;
+  margin-bottom: 1px;
+`
