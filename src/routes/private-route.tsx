80d022477ecbc71eee9fb2e65eeb3238caee7f99
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-10-13 19:43:48 -0300

    [+] added private

---
+++ src/routes/private-route.tsx
@@ -0,0 +1,10 @@
+import React, { ReactNode } from 'react'
+import { Navigate } from 'react-router-dom'
+
+const PrivateRoute = ({ children }: any) => {
+  const user = localStorage.getItem('car-sell:userData')
+
+  return user ? children : <Navigate to="/login" />
+}
+
+export default PrivateRoute
