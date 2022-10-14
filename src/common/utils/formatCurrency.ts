1d7e73b0b6cebca25c49c4f80b98f52223d1c702
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-10-14 10:55:45 -0300

    [*] updated util

--- src/common/utils/formatCurrency.ts
+++ src/common/utils/formatCurrency.ts
@@ -1,8 +1,17 @@
-const formatCurrency = (value: any) => {
-  return new Intl.NumberFormat('pt-BR', {
-    style: 'currency',
-    currency: 'BRL',
-  }).format(value)
-}
+export function maskCurrencyInput(event: any) {
+  const onlyDigits = event.target.value
+    .split('')
+    .filter((s: any) => /\d/.test(s))
+    .join('')
+    .padStart(3, '0')
+  const digitsFloat = `${onlyDigits.slice(0, -2)}.${onlyDigits.slice(-2)}`
 
-export default formatCurrency
+  function maskCurrency(valor: any, locale = 'pt-BR', currency = 'BRL') {
+    return new Intl.NumberFormat(locale, {
+      style: 'currency',
+      currency,
+    }).format(valor)
+  }
+
+  event.target.value = maskCurrency(digitsFloat)
+}
