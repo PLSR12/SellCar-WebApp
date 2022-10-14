5a72729eb8bcf071a5e69f342dc43a214f9a166f
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-10-14 10:56:31 -0300

    [*] added routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,37 @@ import React from 'react'
 import { Route, Routes as RoutesWrapper } from 'react-router-dom'
 
 import * as Pages from '../pages'
+import PrivateRoute from './private-route'
 
 export default function Routes() {
   return (
     <RoutesWrapper>
       <Route path="/" element={<Pages.Home />} />
       <Route path="/login" element={<Pages.Login />} />
+      <Route
+        path="/meus-anuncios"
+        element={
+          <PrivateRoute>
+            <Pages.MyAds />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/minha-conta"
+        element={
+          <PrivateRoute>
+            <Pages.MyAccount />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/anunciar"
+        element={
+          <PrivateRoute>
+            <Pages.CarRegister />
+          </PrivateRoute>
+        }
+      />
     </RoutesWrapper>
   )
 }
