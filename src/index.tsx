08fc3cefcf73a3186a4c775670169ce786bb3969
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-10-17 11:04:59 -0300

    [*] deleted react setrict mode

--- src/index.tsx
+++ src/index.tsx
@@ -7,8 +7,6 @@ import App from './App'
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <App />
   </BrowserRouter>
 )
